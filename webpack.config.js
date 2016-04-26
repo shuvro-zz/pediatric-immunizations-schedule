3da9ed8e4ea4c943e28024246d78f74f4dfcfdff
Eliot Kristan <eliot.k@gmail.com>
2016-04-26 14:40:23 -0400

    Use babel for webpack
    Since we're now using some ES6 syntax, we need to use the babel loader to create
    the dist file

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,15 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
     ]
   }
 };
